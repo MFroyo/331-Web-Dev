a3132efbeb20bd4ff461bf1ccd51967439961ad5
MFroyo <70235595+MFroyo@users.noreply.github.com>
2023-09-23 16:47:11 -0600

    Added Error Handling

--- TempConvertStarter/scripts/convert_temp.js
+++ TempConvertStarter/scripts/convert_temp.js
@@ -8,39 +8,52 @@ function domLoaded() {
    C_in.addEventListener("input",()=>{
       F_in.value="";
    })
-   convertButton.addEventListener("click",(p)=>{
-      p.preventDefault();
+   convertButton.addEventListener("click",(b)=>{
 
-      if(F_in.value !== ''){
+      //If there is a error message from previous atttempt, this clears it
+      document.getElementById("message").textContent = "";
+
+      b.preventDefault();
+
+      //Creates error message and changes text of error message to displayed message below
+   
+
+      if(F_in.value != ''){
          const F = parseFloat(F_in.value);
+
+         //Cancels out with error message if not valid input
+      
+
+        //Sends # to F to C
          const C = convertFtoC(F);
          C_in.value = C; 
 
          displayIcon(F);
+         return; 
       }
       
-      else if (C_in.value !== '') {
+      else if (C_in.value != '') {
          const C = parseFloat(C_in.value);
+
+         //Cancels out with error message if not valid input
+
+        //Sends # to C to F
          const F = convertCtoF(C);
          F_in.value = F; 
 
-         displayIcon(F);  
+         displayIcon(F);
+         return;  
      }
- })
-   // TODO: Use addEventListener() to register a click event handler for the convert button.
-   // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#add_a_simple_listener
-   // Add event listeners to handle clearing the box that WAS NOT clicked,
-   // e.g., the element C_in listens for 'input', with a callback fn to
-   // execute after that event does happen. 
-   // You don't send arguments to the event handler function.
-   // So, if you want the event handler to call another function that
-   // DOES take arguments, you can send that other function as a callback.
-   // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#event_listener_with_anonymous_function
-   // Here is an example of anonymous event handler fn that calls alert with an argument:
-   // document.getElementById("weatherIcon").addEventListener("click", function() {alert("You clicked the icon.")})
 
+     //casts error message and changes icon
+     else {
+         document.getElementById("message").textContent = "Enter a temperature to convert,";
+         document.getElementById("weatherIcon").src="./images/C-F.png";
+         return;
+   }
+ })
 }
-// TODO: (Part of the above is to write the functions to be executed when the event handlers are invoked.)
+
 
 function convertCtoF(C) {
    const F = C * 9/5 + 32;
@@ -52,32 +65,24 @@ function convertFtoC(F) {
    return C;
 }
 
+
+
 function displayIcon(F){
-   //alert("Gets here");
    if(F>=200 || F<=-200){
-      //TODO Display DEAD
-      //alert("Gets here");
+      //Display DEAD
       document.getElementById("weatherIcon").src="./images/dead.png";
    }
    else if(F<=32){
-      //TODO Display COLD
+      //Display COLD
       document.getElementById("weatherIcon").src="./images/cold.png";
    }
    else if(F>=90){
-      //TODO Display HOT
+      //Display HOT
       document.getElementById("weatherIcon").src="./images/hot.png";
    }
    else if(F>32 && F<90){
-      //TODO Display COOL
+      //Display COOL
       document.getElementById("weatherIcon").src="./images/cool.png";
 
    }
 }
-// TODO: write a fn that can be called with every temp conversion
-// to display the correct weather icon.
-// Based on degrees Fahrenheit:
-// 32 or less, but above -200: cold
-// 90 or more, but below 200: hot
-// between hot and cold: cool
-// 200 or more, -200 or less: dead
-// both input fields are blank: C-F
